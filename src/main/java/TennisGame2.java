9c1619c3e34a370b0b7fea11f3f63751d40125b9
Kaan B. Y. <109346960+6b6279@users.noreply.github.com>
2026-06-25 14:33:49 +0000

    Initial commit

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,9 +1,8 @@
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
-    
+
     public String P1res = "";
     public String P2res = "";
     private String player1Name;
@@ -14,115 +13,92 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    private String evaluateIntermittentDraw(int p1Score, int p2Score) {
+        if (p1Score == p2Score) {
+            if (p1Score < 3) {
+                return inWords(p1Score) + "-All";
+            } else {
+                return "Deuce";
+            }
+        }
+
+        return "";
+    }
+
+    private String evaluateIntermittentNonDraw(int p1Score, int p2Score) {
+        if ((p1Score < 4 && p2Score < 4) && p1Score != p2Score) {
+            return inWords(p1Score) + "-" + inWords(p2Score);
+        }
+
+        return "";
+    }
+
+    private String evaluateAdvantage(int p1Score, int p2Score) {
+        if (p1Score >= 3 && p2Score >= 3) {
+            if (p1Score == p2Score + 1) {
+                return "Advantage " + player1Name;
+            } else if (p2Score == p1Score + 1) {
+                return "Advantage " + player2Name;
+            }
+        }
+
+        return "";
+    }
+
+    private String evaluateWin(int p1Score, int p2Score) {
+        if (p1Score >= 4 || p2Score >= 4) {
+            if (p1Score >= p2Score + 2) {
+                return "Win for " + player1Name;
+            } else if (p2Score >= p1Score + 2) {
+                return "Win for " + player2Name;
+            }
+        }
+
+        return "";
+    }
+
+    public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1";
-        }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
-        }
+
+        score += evaluateWin(P1point, P2point);
+        score += evaluateIntermittentDraw(P1point, P2point);
+        score += evaluateIntermittentNonDraw(P1point, P2point);
+        score += evaluateAdvantage(P1point, P2point);
+
         return score;
     }
-    
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
+
+    private String inWords(int points) {
+        return switch (points) {
+            case 0 -> "Love";
+            case 1 -> "Fifteen";
+            case 2 -> "Thirty";
+            case 3 -> "Forty";
+            default -> throw new IllegalArgumentException("Nonesuch legal score");
+        };
+    }
+
+    public void SetP1Score(int number) {
+
+        for (int i = 0; i < number; i++) {
             P1Score();
         }
-            
+
     }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
+
+    public void SetP2Score(int number) {
+
+        for (int i = 0; i < number; i++) {
             P2Score();
         }
-            
+
     }
-    
-    public void P1Score(){
+
+    public void P1Score() {
         P1point++;
     }
-    
-    public void P2Score(){
+
+    public void P2Score() {
         P2point++;
     }
 
